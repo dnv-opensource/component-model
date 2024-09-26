0541341efa077caff91f1093ef17b2a45f2f6a3a
Mendez <Jorge.Luis.Mendez@dnv.com>
2024-09-26 16:06:14 +0200

    Add new changes to bouncingball fmu

--- tests/examples/bouncing_ball_3d.py
+++ tests/examples/bouncing_ball_3d.py
@@ -1,6 +1,7 @@
 from math import sqrt
 
 import numpy as np
+
 from component_model.model import Model
 from component_model.variable import Variable
 
@@ -27,84 +28,98 @@ class BouncingBall3D(Model):
         self,
         name: str = "BouncingBall3D",
         description="Another BouncingBall model, made in Python and using Model and Variable to construct a FMU",
-        pos: tuple = (0, 0, 10),
-        speed: tuple = (1, 0, 0),
-        g: float = 9.81,
+        pos: tuple = ("0 m", "0 m", "10 m"),
+        speed: tuple = ("1 m/s", "0 m/s", "0 m/s"),
+        g: float = "9.81 m/s^2",
         e: float = 0.9,
         min_speed_z: float = 1e-6,
         **kwargs,
     ):
         super().__init__(name, description, author="DNV, SEACo project", **kwargs)
-        self.pos = np.array(pos, dtype=float)
-        self.speed = np.array(speed, dtype=float)
-        self.a = np.array((0, 0, -g), float)
-        self.g = g
-        self.e = e
+        self._pos = self._interface( 'pos', pos)
+        self._speed = self._interface( 'speed', speed)
+        self._g = self._interface( 'g', g)
+        self.a = np.array((0, 0, -self.g), float)
+        self._e = self._interface( 'e', e)
         self.min_speed_z = min_speed_z
         self.stopped = False
         self.time = 0.0
+        self._p_bounce = self._interface( 'p_bounce', ('0m', '0m','0m')) # instantiates self.p_bounce. z always 0.
         self.t_bounce, self.p_bounce = self.next_bounce()
-        self._interface_variables()
 
-    def _interface_variables(self):
-        """Define the FMU2 interface variables, using the variable interface."""
-        self._pos = Variable(
-            self,
-            name="pos",
-            description="The 3D position of the ball [m] (height in inch as displayUnit example.",
-            causality="output",
-            variability="continuous",
-            initial="exact",
-            start=(str(self.pos[0]) + "m", str(self.pos[1]) + "m", str(self.pos[2]) + "inch"),
-            rng=((0, "100 m"), None, (0, "10 m")),
-        )
-        self._speed = Variable(
-            self,
-            name="speed",
-            description="The 3D speed of the ball, i.e. d pos / dt [m/s]",
-            causality="output",
-            variability="continuous",
-            initial="exact",
-            start=tuple(str(x) + "m/s" for x in self.speed),
-            rng=((0, "1 m/s"), None, ("-100 m/s", "100 m/s")),
-        )
-        self._g = Variable(
-            self,
-            name="g",
-            description="The gravitational acceleration (absolute value).",
-            causality="parameter",
-            variability="fixed",
-            start=str(self.g) + "m/s^2",
-            rng=(),
-        )
-        self._e = Variable(
-            self,
-            name="e",
-            description="The coefficient of restitution, i.e. |speed after| / |speed before| bounce.",
-            causality="parameter",
-            variability="fixed",
-            start=self.e,
-            rng=(),
-        )
-        self._p_bounce = Variable(
-            self,
-            name="p_bounce",
-            description="The expected position of the next bounce as 3D vector",
-            causality="output",
-            variability="continuous",
-            start=tuple(str(x) for x in self.p_bounce),
-            rng=(),
-        )
+    def _interface(self, name:str, start:float|tuple):
+        """Define a FMU2 interface variable, using the variable interface.
+
+        Args:
+            name (str): base name of the variable
+            start (str|float|tuple): start value of the variable (optionally with units)
+        
+        Returns:
+            the variable object. As a side effect the variable value is made available as self.<name>
+        """
+        if name == 'pos':
+            return Variable(
+                self,
+                name="pos",
+                description="The 3D position of the ball [m] (height in inch as displayUnit example.",
+                causality="output",
+                variability="continuous",
+                initial="exact",
+                start=start,
+                rng=((0, "100 m"), None, (0, "10 m")),
+            )
+        elif name == 'speed':
+            return Variable(
+                self,
+                name="speed",
+                description="The 3D speed of the ball, i.e. d pos / dt [m/s]",
+                causality="output",
+                variability="continuous",
+                initial="exact",
+                start=start,
+                rng=((0, "1 m/s"), None, ("-100 m/s", "10 m/s")),
+            )
+        elif name == 'g':
+            return Variable(
+                self,
+                name="g",
+                description="The gravitational acceleration (absolute value).",
+                causality="parameter",
+                variability="fixed",
+                start=start,
+                rng=(),
+            )
+        elif name == 'e':
+            return Variable(
+                self,
+                name="e",
+                description="The coefficient of restitution, i.e. |speed after| / |speed before| bounce.",
+                causality="parameter",
+                variability="fixed",
+                start=start,
+                rng=(),
+            )
+        elif name == 'p_bounce':
+            return Variable(
+                self,
+                name="p_bounce",
+                description="The expected position of the next bounce as 3D vector",
+                causality="output",
+                variability="continuous",
+                start=start,
+                rng=(),
+            )
 
     def do_step(self, time, dt):
         """Perform a simulation step from `time` to `time + dt`."""
         if not super().do_step(time, dt):
             return False
-        self.t_bounce, self.p_bounce = self.next_bounce()
-        while dt > self.t_bounce:  # if the time is this long
-            dt -= self.t_bounce
+        self.dt_bounce, self.p_bounce = self.next_bounce()
+        # print(f"Step@{time}. pos:{self.pos}, speed{self.speed}, dt_bounce:{self.dt_bounce}, p_bounce:{self.p_bounce}")
+        while dt > self.dt_bounce:  # if the time is this long
+            dt -= self.dt_bounce
             self.pos = self.p_bounce
-            self.speed -= self.a * self.t_bounce  # speed before bouncing
+            self.speed += self.a * self.dt_bounce  # speed before bouncing
             self.speed[2] = -self.speed[2]  # speed after bouncing if e==1.0
             self.speed *= self.e  # speed reduction due to coefficient of restitution
             if self.speed[2] < self.min_speed_z:
@@ -112,28 +127,31 @@ class BouncingBall3D(Model):
                 self.a[2] = 0.0
                 self.speed[2] = 0.0
                 self.pos[2] = 0.0
-            self.t_bounce, self.p_bounce = self.next_bounce()
-        self.speed += self.a * dt
+            self.time += self.dt_bounce # jump to the exact bounce time
+            self.dt_bounce, self.p_bounce = self.next_bounce() # update to the next bounce
         self.pos += self.speed * dt + 0.5 * self.a * dt**2
+        self.speed += self.a * dt
         if self.pos[2] < 0:
             self.pos[2] = 0
-        # print(f"@{time}. pos {self.pos}, speed {self.speed}, bounce {self.t_bounce}")
+        self.time += dt
         return True
 
     def next_bounce(self):
         """Calculate time until next bounce and position where the ground will be hit,
-        based on current time, pos and speed.
+        based on pos and speed.
         """
         if self.stopped:  # stopped bouncing
             return (1e300, np.array((1e300, 1e300, 0), float))
             # return ( float('inf'), np.array( (float('inf'), float('inf'), 0), float))
         else:
-            t_bounce = (self.speed[2] + sqrt(self.speed[2] ** 2 + 2 * self.g * self.pos[2])) / self.g
-            p_bounce = self.pos + self.speed * t_bounce  # linear. not correct for z-direction!
+            dt_bounce = (self.speed[2] + sqrt(self.speed[2] ** 2 + 2 * self.g * self.pos[2])) / self.g
+            p_bounce = self.pos + self.speed * dt_bounce  # linear. not correct for z-direction!
             p_bounce[2] = 0
-            return (t_bounce, p_bounce)
+            return (dt_bounce, p_bounce)
 
     def setup_experiment(self, start: float):
         """Set initial (non-interface) variables."""
         super().setup_experiment(start)
         self.stopped = False
+        self.a = np.array((0, 0, -self.g), float)
+
--- tests/test_bouncing_ball_3d.py
+++ tests/test_bouncing_ball_3d.py
@@ -1,9 +1,12 @@
+from math import sqrt
 import time
 import xml.etree.ElementTree as ET  # noqa: N817
+import matplotlib.pyplot as plt
 from pathlib import Path
 from zipfile import ZipFile
 
 import pytest
+from tests.examples.bouncing_ball_3d import BouncingBall3D  # type: ignore
 from component_model.model import Model  # type: ignore
 from component_model.utils import model_from_fmu
 from fmpy import simulate_fmu  # type: ignore
@@ -13,26 +16,33 @@ from libcosimpy.CosimEnums import CosimExecutionState
 from libcosimpy.CosimExecution import CosimExecution
 from libcosimpy.CosimSlave import CosimLocalSlave
 
-from tests.examples.bouncing_ball_3d import BouncingBall3D  # type: ignore
-
 
 def _in_interval(x: float, x0: float, x1: float):
     return x0 <= x <= x1 or x1 <= x <= x0
 
 
-def arrays_equal(arr1, arr2, dtype="float64", eps=1e-7):
+def arrays_equal(arr1, arr2, eps=1e-7):
     assert len(arr1) == len(arr2), "Length not equal!"
 
     for i in range(len(arr1)):
         # assert type(arr1[i]) == type(arr2[i]), f"Array element {i} type {type(arr1[i])} != {type(arr2[i])}"
         assert abs(arr1[i] - arr2[i]) < eps, f"Component {i}: {arr1[i]} != {arr2[i]}"
 
+def show_data( _z):
+    fig, ax = plt.subplots()
+    ax.plot(_z)
+    plt.title("Data (_z)", loc="left")
+    plt.show()
 
 def _to_et(file: str, sub: str = "modelDescription.xml"):
     with ZipFile(file) as zp:
         xml = zp.read(sub)
     return ET.fromstring(xml)
 
+def result( bb):
+    '''Make a row of the fmpy results vector (all output variables in display units)'''
+    return (bb.time, *bb._pos.getter(), *bb._speed.getter(), *bb._p_bounce.getter())
+    
 
 @pytest.fixture(scope="session")
 def bouncing_ball_fmu():
@@ -47,19 +57,83 @@ def bouncing_ball_fmu():
 
 
 def test_bouncing_ball_class():
-    bb = BouncingBall3D(pos=(0, 0, 10), speed=(1, 0, 0), g=9.81, e=0.9, min_speed_z=1e-6)
-    arrays_equal(bb.pos, (0, 0, 0.254))  # 1)) was provided as inch
+    bb = BouncingBall3D(
+        pos=('0 m', '0 m', '10 inch'),
+        speed=('1 m/s', '0 m/s', '0 m/s'),
+        g='9.81 m/s^2',
+        e=0.9,
+        min_speed_z=1e-6)
+    arrays_equal(bb.pos, (0, 0, 10*0.0254))  # was provided as inch
+    arrays_equal(bb.speed, (1,0,0))
     assert bb.g == 9.81
+    assert bb.e == 0.9
+    t_bounce = sqrt(2*10*0.0254 / 9.81)
+    v_bounce = 9.81 * t_bounce # speed in z-direction
+    x_bounce = t_bounce # x-position where it bounces in m
+    arrays_equal( bb.p_bounce, (x_bounce,0,0))
     time = 0
     dt = bb.default_experiment["stepSize"]
+    assert dt==0.01
+    # set start values (in display units. Are translated to internal units
+    bb._pos.setter( (0,0,10))
     height = [bb.pos[2]]
     times = [0]
-    for _ in range(200):
+    t_b, p_b = bb.next_bounce()
+    assert t_bounce==t_b
+    arrays_equal( (x_bounce, 0, 0), p_b), f"x_bounce:{x_bounce} != {p_b[0]}" 
+    z = [bb._pos.getter()[2]]
+    # after one step
+    bb.do_step(time, dt)
+    z.append( bb._pos.getter()[2])
+    arrays_equal(result(bb), (0.01,
+                      0.01, 0, (10*0.0254-0.5*9.81*0.01**2)/0.0254,
+                      1, 0, -9.81*0.01, sqrt(2*10*0.0254/9.81), 0, 0))
+    # just before bounce
+    t_before = int(t_bounce / dt) * dt  # just before bounce
+    assert t_before == 0.22
+    for _ in range(int(t_bounce / dt)-1):
         bb.do_step(time, dt)
-        time += dt
-        height.append(bb.pos[2])
-        times.append(time)
-
+        z.append( bb._pos.getter()[2])
+    arrays_equal(
+        result(bb),
+        (t_before, 1*t_before, 0, (10*0.0254 - 0.5 * 9.81 * t_before**2)/0.0254, 1, 0, -9.81 * t_before, x_bounce, 0, 0),
+        eps=0.003,
+    )
+    # just after bounce
+    bb.do_step( time, dt)
+    z.append( bb._pos.getter()[2])
+    ddt = (t_before + dt - t_bounce) # time from bounce to end of step
+    t_bounce2 = t_bounce + 2* v_bounce*0.9/ 9.81
+    x_bounce2 = x_bounce + 2* v_bounce*0.9* 1.0*0.9/ 9.81
+    arrays_equal(
+        result(bb), ( t_before + dt,
+                      t_bounce*1 + 1*0.9*ddt, 0, (v_bounce*0.9*ddt  - 0.5*9.81*ddt**2)/0.0254,
+                      0.9*1, 0, (v_bounce * 0.9 - 9.81 * ddt),
+                      x_bounce2, 0, 0),
+        eps=0.03)
+#     # from bounce to bounce
+#     v_x, v_z, t_b, x_b = 1.0, v_bounce, t_bounce, x_bounce
+#     for n in range(2, 100): # from bounce to bounce
+#         v_x = v_x* 0.9
+#         v_z = v_z* 0.9
+#         delta_t = 2* v_z* v_x/ 9.81
+#         t_b = t_b + delta_t
+#         x_b = x_b + v_x* t_b
+#     
+#         for _ in range( int( delta_t/dt)):
+#             bb.do_step( time, dt)
+#             z.append( bb._pos.getter()[2])
+#         
+#         print( f"Bounce {n}: {bb.pos}, steps:{len(z)}")
+#    show_data(z)
+#     return
+#     arrays_equal(result[int(2.5 / dt)], (2.5, 0, 0), eps=0.4)
+#     arrays_equal(result[int(3 / dt)], (3, 0, 0))
+#     print("RESULT", result[int(t_before / dt) + 1])
+#     
+#     time += dt
+#     height.append(bb.pos[2])
+#     times.append(time)
 
 def test_make_bouncing_ball(bouncing_ball_fmu):
     _ = fmu_info(bouncing_ball_fmu)  # not necessary, but it lists essential properties of the FMU
